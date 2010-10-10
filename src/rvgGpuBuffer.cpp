ae21a0a7f83a70b79ea07746bc47aeee2be09abc
Ivan Leben <ivan.leben@gmail.com>
2010-10-10 20:51:50 +1100

    Yeah... maybe add files for GpuBuffer lol.

---
+++ src/rvgGpuBuffer.cpp
@@ -0,0 +1,138 @@
+#include "rvgMain.h"
+
+GpuBuffer::GpuBuffer (BufferType type)
+{
+  this->id = 0;
+  this->idValid = false;
+
+  this->addr = 0;
+  this->addrValid = false;
+
+  this->type = type;
+  this->bufSize = 0;
+  this->dataSize = 0;
+}
+
+Uint64 GpuBuffer::capacity ()
+{
+  return bufSize;
+}
+
+/*
+Ensures the buffer has at least newBufSize memory available.
+Buffer contents are invalidated. */
+
+bool GpuBuffer::reserve (Uint64 newBufSize)
+{
+  //Check invalid size
+  if (newBufSize == 0)
+    return true;
+
+  //Generate buffer first time
+  if (!idValid)
+  {
+    glGenBuffers( 1, &id );
+    idValid = true;
+  }
+
+  //Check if resize is required at all
+  if (newBufSize > bufSize)
+  {
+    //Unload buffer if it was made resident
+    if (addrValid)
+    {
+      glBindBuffer( GL_ARRAY_BUFFER, id );
+      glMakeBufferNonResident( GL_ARRAY_BUFFER );
+      addrValid = false;
+    }
+
+    //Setup buffer size
+    switch (type) {
+    case GpuBuffer::Static:
+      glBindBuffer( GL_ARRAY_BUFFER, id );
+      glBufferData( GL_ARRAY_BUFFER, (GLsizeiptr) newBufSize, NULL, GL_STATIC_DRAW );
+      break;
+    case GpuBuffer::Dynamic:
+      glBindBuffer( GL_ARRAY_BUFFER, id );
+      glBufferData( GL_ARRAY_BUFFER, (GLsizeiptr) newBufSize, NULL, GL_DYNAMIC_DRAW );
+      break;
+    }
+
+    bufSize = newBufSize;
+  }
+
+  dataSize = 0;
+  return true;
+}
+
+/*
+Appends data to the end of the buffer. Returns false if not enough
+memory available. */
+
+bool GpuBuffer::append (Uint64 newDataSize, void *data)
+{
+  //Check invalid size
+  if (newDataSize == 0)
+    return true;
+
+  //Check if memory available
+  if (dataSize + newDataSize > bufSize)
+    return false;
+
+  //Upload at end of existing data
+  glBindBuffer( GL_ARRAY_BUFFER, id );
+  glBufferSubData( GL_ARRAY_BUFFER, (GLsizeiptr) dataSize, (GLsizeiptr) newDataSize, data );
+
+  dataSize += newDataSize;
+  return true;
+}
+
+/*
+Uploads the data into the buffer, allocating memory if required.
+Previous buffer contents are overwritten. Same as calling
+reserve() followed by append(). */
+
+bool GpuBuffer::upload (Uint64 newDataSize, void *data)
+{
+  //Check invalid size
+  if (newDataSize == 0)
+    return true;
+
+  //Make sure memory is available
+  if (! reserve( newDataSize ))
+    return false;
+
+  //Upload at start of buffer
+  glBindBuffer( GL_ARRAY_BUFFER, id );
+  glBufferSubData( GL_ARRAY_BUFFER, 0, (GLsizeiptr) newDataSize, data );
+
+  dataSize = newDataSize;
+  return true;
+}
+
+/*
+Invalidates buffer contents */
+
+void GpuBuffer::clear()
+{
+  dataSize = 0;
+}
+
+GLuint GpuBuffer::getId ()
+{
+  return id;
+}
+
+GLuint64 GpuBuffer::getAddress ()
+{
+  //Make buffer resident and query address
+  if (!addrValid)
+  {
+    glBindBuffer( GL_ARRAY_BUFFER, id );
+    glMakeBufferResident( GL_ARRAY_BUFFER, GL_READ_WRITE );
+    glGetBufferParameterui64v( GL_ARRAY_BUFFER, GL_BUFFER_GPU_ADDRESS_NV, &addr );
+    addrValid = true;
+  }
+
+  return addr;
+}
---
+++ src/rvgGpuBuffer.h
@@ -0,0 +1,46 @@
+#ifndef RVGGPUBUFFER_H
+#define RVGGPUBUFFER_H 1
+
+class GpuBuffer
+{
+public:
+  enum BufferType
+  {
+    Static,
+    Dynamic
+  };
+
+private:
+  BufferType type;
+
+  GLuint id;
+  bool idValid;
+
+  GLuint64 addr;
+  bool addrValid;
+
+  Uint64 bufSize;
+  Uint64 dataSize;
+
+public:
+  GpuBuffer (BufferType type);
+
+  Uint64 capacity();
+  bool reserve (Uint64 newBufSize);
+  bool append (Uint64 newDataSize, void *data);
+  bool upload (Uint64 newDataSize, void *data);
+  void clear();
+
+  GLuint getId ();
+  GLuint64 getAddress ();
+};
+
+class StaticGpuBuffer : public GpuBuffer {
+public: StaticGpuBuffer() : GpuBuffer( GpuBuffer::Static ) {}
+};
+
+class DynamicGpuBuffer : public GpuBuffer {
+public: DynamicGpuBuffer() : GpuBuffer( GpuBuffer::Dynamic ) {}
+};
+
+#endif//RVGGPUBUFFER_H
